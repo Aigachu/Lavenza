a693d20eba258cfe3de8fb8b6932950b7527c663
Aigachu <aigachu.sama@gmail.com>
2019-10-28 13:26:52 -0400

    Alright fine then! How abou this?

--- core/lib/Lavenza.d.ts
+++ core/lib/Lavenza.d.ts
@@ -1,3 +1,5 @@
+/* tslint:disable:completed-docs only-arrow-functions max-classes-per-file */
+
 /**
  * Project Lavenza
  * Copyright 2017-2018 Aigachu, All Rights Reserved
@@ -8,38 +10,47 @@ declare module "lavenza" {
   // === Imports ===
   import * as path from "path";
 
-  import { BotManager } from "./Lavenza/Bot/BotManager";
-  import { ClientType } from "./Lavenza/Bot/Client/ClientType";
-  import { CommandClientHandler } from "./Lavenza/Bot/Command/CommandClientHandler";
+  import { CommandConfigurations } from "./Lavenza/Bot/Command/CommandConfigurations";
   import { Listener } from "./Lavenza/Bot/Listener/Listener";
-  import { PromptException } from "./Lavenza/Bot/Prompt/Exception/PromptException";
-  import { PromptExceptionType } from "./Lavenza/Bot/Prompt/Exception/PromptExceptionType";
   import { Resonance } from "./Lavenza/Bot/Resonance/Resonance";
-  import { Akechi } from "./Lavenza/Confidant/Akechi";
-  import { Igor } from "./Lavenza/Confidant/Igor";
-  import { Kawakami } from "./Lavenza/Confidant/Kawakami";
-  import { Morgana } from "./Lavenza/Confidant/Morgana";
-  import { Sojiro } from "./Lavenza/Confidant/Sojiro";
-  import { Yoshida } from "./Lavenza/Confidant/Yoshida";
   import { Core } from "./Lavenza/Core/Core";
-  import { Gestalt } from "./Lavenza/Gestalt/Gestalt";
-  import { Talent } from "./Lavenza/Talent/Talent";
-  import { TalentManager } from "./Lavenza/Talent/TalentManager";
-  import { AbstractObject } from "./Lavenza/Types";
+  import { TalentConfigurations } from "./Lavenza/Talent/TalentConfigurations";
+  import { AssociativeObject } from "./Lavenza/Types";
 
   // === Classes ===
-  export { Core } from "./Lavenza/Core/Core";
-  export { Command } from "./Lavenza/Bot/Command/Command";
+  export class Command {
+    public id: string;
+    public id: string;
+    public key: string;
+    public directory: string;
+    public config: CommandConfigurations;
+    protected talent: Talent;
+    public async fireClientHandlers(resonance: Resonance, data: AbstractObject | string, method: string = "execute"): Promise<unknown>;
+  }
+
+  export class Talent {
+    public commandAliases: AssociativeObject<string> = {};
+    public commands: AssociativeObject<Command> = {};
+    public config: TalentConfigurations;
+    public databases: AssociativeObject<string> = {};
+    public directory: string;
+    public listeners: Listener[] = [];
+    public machineName: string;
+  }
 
   // === Types ===
-  export { AbstractObject };
+  interface AbstractObject {
+    // tslint:disable-next-line:no-any
+    [key: string]: any;
+  }
+  interface AssociativeObject<T> {
+    [key: string]: T;
+  }
 
   // === Functions ===
-  // tslint:disable-next-line:only-arrow-functions completed-docs
   export async function initialize(root: string = path.dirname(require.main.filename)): Promise<Core> {
     return Core.initialize(root);
   }
-  // tslint:disable-next-line:only-arrow-functions completed-docs
   export async function summon(): Promise<void> {
     return Core.summon();
   }
