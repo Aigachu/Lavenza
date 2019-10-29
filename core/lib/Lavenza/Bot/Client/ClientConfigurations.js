3994435c32a951568ae79bfbaaacf698f88cfd06
Aigachu <aigachu.sama@gmail.com>
2019-10-29 12:41:20 -0400

    Removing .js from repository.

--- core/lib/Lavenza/Bot/Client/ClientConfigurations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Project Lavenza
- * Copyright 2017-2019 Aigachu, All Rights Reserved
- *
- * License: https://github.com/Aigachu/Lavenza-II/blob/master/LICENSE
- */
-Object.defineProperty(exports, "__esModule", { value: true });
