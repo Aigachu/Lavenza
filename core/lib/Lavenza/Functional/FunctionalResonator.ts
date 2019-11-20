55f9f9cde4a3cc550d11c1886d2f1df2ed232da2
Aigachu <aigachu.sama@gmail.com>
2019-11-20 18:29:38 -0500

    Separated functionality for functional door. Created a TalentResonator as it is more appropriate.

--- core/lib/Lavenza/Functional/FunctionalResonator.ts
+++ core/lib/Lavenza/Functional/FunctionalResonator.ts
@@ -9,8 +9,7 @@
 import { Akechi } from "../Confidant/Akechi";
 import { Resonance } from "../Resonance/Resonance";
 import { Resonator } from "../Resonance/Resonator/Resonator";
-import { ServiceContainer } from "../Service/ServiceContainer";
-import { TalentCatalogue } from "../Talent/TalentCatalogue";
+
 import { BotFunctionalDoor } from "./BotFunctionalDoor";
 
 /**
@@ -27,19 +26,6 @@ export class FunctionalResonator extends Resonator {
    * Each resonator service must implement this function to determine what to do upon hearing a resonance.
    */
   public async resonate(resonance: Resonance): Promise<void> {
-    // Get all talents assigned to this bot.
-    const talents = await ServiceContainer.get(TalentCatalogue).getTalentsForBot(resonance.bot);
-
-    // We want to sort each talent by priority real fast.
-    talents.sort((a, b) => b.priority - a.priority);
-
-    // We'll loop through all Talents assigned to this bot.
-    for (const talent of talents) {
-      // Run the Resonate function for the talent.
-      // Not all talents will implement this, but they can if they want to.
-      await talent.resonate(resonance);
-    }
-
     // Otherwise, check if a functional file exists for the bot.
     const botFunctDoor = `${resonance.bot.directory}/${resonance.bot.id}.js`;
     if (await Akechi.fileExists(botFunctDoor)) {
---
+++ core/lib/Lavenza/Talent/TalentResonator.ts
@@ -0,0 +1,45 @@
+/**
+ * Project Lavenza
+ * Copyright 2017-2019 Aigachu, All Rights Reserved
+ *
+ * License: https://github.com/Aigachu/Lavenza-II/blob/master/LICENSE
+ */
+
+// Imports.
+import { Resonance } from "../Resonance/Resonance";
+import { Resonator } from "../Resonance/Resonator/Resonator";
+import { ServiceContainer } from "../Service/ServiceContainer";
+
+import { TalentCatalogue } from "./TalentCatalogue";
+
+/**
+ * Provides a Resonator for Talents.
+ */
+export class TalentResonator extends Resonator {
+
+  /**
+   * The priority of the Resonator. This determines the order in which Resonators will resonate.
+   */
+  public priority: number = 4800;
+
+  /**
+   * Each resonator service must implement this function to determine what to do upon hearing a resonance.
+   */
+  public async resonate(resonance: Resonance): Promise<void> {
+    // Get all talents assigned to this bot.
+    const talents = await ServiceContainer.get(TalentCatalogue).getTalentsForBot(resonance.bot);
+
+    // We want to sort each talent by priority real fast.
+    talents.sort((a, b) => b.priority - a.priority);
+
+    // We'll loop through all Talents assigned to this bot.
+    for (const talent of talents) {
+      // Run the Resonate function for the talent.
+      // Not all talents will implement this, but they can if they want to.
+      await talent.resonate(resonance);
+    }
+  }
+
+}
+
+
